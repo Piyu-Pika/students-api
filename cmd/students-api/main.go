86eb03b17f265ea8819a02d360370d04906242a5
piyush bhardwaj <piyushbhardwaj1603@gmail.com>
2025-02-28 17:31:55 +0530

    implement basic HTTP server and configuration loading for Students API

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Piyu-Pika/students-api/internal/config"
+)
 
 func main() {
 	fmt.Println("Welcome to Students API")
+	//LoadConfig
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+	//database setup
+	// router setup
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Students API"))
+		fmt.Fprintf(w, "Hello World")
+	})
+	// setup server
+
+	server := &http.Server{
+		Addr:    cfg.HttpServer.Address,
+		Handler: router,
+	}
+	log.Println("Server started")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 }
